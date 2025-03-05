f513ba3752b385d968b9145a9cb663e631eea7c6
kaiwen.lau <kaiwen.lau@ufinity.com>
2025-03-06 06:41:29 +0800

    added code for user stories 1

---
+++ typescript/src/controllers/RegistrationController.ts
@@ -0,0 +1,216 @@
+import Express, { RequestHandler, response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import Sequelize from 'sequelize';
+import sequelize from '../config/database';
+import Logger from '../config/logger';
+import defineTeacher from '../model/TeacherModel';
+import defineStudent from '../model/StudentModel';
+import defineSubject from '../model/SubjectModel';
+import defineClass from '../model/ClassModel';
+import defineTeacherSubject from '../model/TeacherSubjectModel';
+import defineTeacherClass from '../model/TeacherClassModel';
+import defineStudentClass from '../model/StudentClassModel';
+
+const RegistrationController = Express.Router();
+const LOG = new Logger('RegistrationController.ts');
+
+type ReqTeacher = {
+  name: string;
+  email: string;
+};
+type ReqStudents = [
+  {
+    name: string;
+    email: string;
+  }
+];
+type ReqSubject = {
+  name: string;
+  subjectCode: string;
+};
+type ReqClass = {
+  name: string;
+  classCode: string;
+};
+
+/**
+ * Create or Update
+ * @param req
+ * @param res
+ * @returns
+ */
+const registrationHandler: RequestHandler = async (req, res) => {
+  try {
+    if (!('teacher' in req.body)) throw '[400] Teacher is required.';
+    if (!('students' in req.body)) throw '[400] Students is required.';
+    if (!('subject' in req.body)) throw '[400] Subject is required.';
+    if (!('class' in req.body)) throw '[400] Class is required.';
+
+    const result = await sequelize.transaction(async () => {
+      const teacherId = await updateTeacher(req.body.teacher);
+      const studentIds = await updateStudents(req.body.students);
+      const subjectId = await updateSubject(req.body.subject);
+      const classId = await updateClass(req.body.class);
+      const teacherSubjectId = await updateTeacherSubject(teacherId, subjectId);
+      await updateTeacherClass(teacherSubjectId, classId);
+      await updateStudentClass(studentIds, classId);
+      res.status(StatusCodes.NO_CONTENT).send('Registration success');
+    });
+  } catch (e) {
+    LOG.error(e);
+    if (e.includes('400')) res.status(StatusCodes.BAD_REQUEST).send(e);
+    else res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(e);
+  }
+};
+
+const updateTeacher = async (reqTeacher: ReqTeacher) => {
+  if (!('name' in reqTeacher)) throw '[400] Teacher name is required.';
+  if (!('email' in reqTeacher)) throw '[400] Teacher email is required.';
+
+  const [teacher, created] = await defineTeacher.findOrCreate({
+    where: { email: reqTeacher.email },
+    defaults: { name: reqTeacher.name },
+  });
+
+  if (created) {
+    LOG.info(`New teacher created.`);
+  } else {
+    await defineTeacher.update(
+      {
+        name: reqTeacher.name,
+      },
+      {
+        where: {
+          email: reqTeacher.email,
+        },
+      }
+    );
+    LOG.info(`Teacher name updated.`);
+  }
+  return teacher.id;
+};
+
+const updateStudents = async (reqStudents: ReqStudents) => {
+  const studentIds: number[] = [];
+  reqStudents.forEach(async (reqStudent) => {
+    if (!('name' in reqStudent)) throw '[400] Student name is required.';
+    if (!('email' in reqStudent)) throw '[400] Student email is required.';
+
+    const [student, created] = await defineStudent.findOrCreate({
+      where: { email: reqStudent.email },
+      defaults: { name: reqStudent.name },
+    });
+    if (created) {
+      LOG.info(`New student created.`);
+    } else {
+      await defineStudent.update(
+        {
+          name: reqStudent.name,
+        },
+        {
+          where: {
+            email: reqStudent.email,
+          },
+        }
+      );
+      LOG.info(`Student name updated.`);
+    }
+    studentIds.push(student.id);
+  });
+  return studentIds;
+};
+
+const updateSubject = async (reqSubject: ReqSubject) => {
+  if (!('name' in reqSubject)) throw '[400] Subject name is required.';
+  if (!('subjectCode' in reqSubject)) throw '[400] Subject code is required.';
+
+  const [subject, created] = await defineSubject.findOrCreate({
+    where: { subjectCode: reqSubject.subjectCode },
+    defaults: { name: reqSubject.name },
+  });
+  if (created) {
+    LOG.info(`New subject created.`);
+  } else {
+    await defineSubject.update(
+      {
+        name: reqSubject.name,
+      },
+      {
+        where: {
+          subjectCode: reqSubject.subjectCode,
+        },
+      }
+    );
+    LOG.info(`Subject name updated.`);
+  }
+  return subject.id;
+};
+
+const updateClass = async (reqClass: ReqClass) => {
+  if (!('name' in reqClass)) throw '[400] Class name is required.';
+  if (!('classCode' in reqClass)) throw '[400] Class code is required.';
+
+  const [classes, created] = await defineClass.findOrCreate({
+    where: { classCode: reqClass.classCode },
+    defaults: { name: reqClass.name },
+  });
+  if (created) {
+    LOG.info(`New class created.`);
+  } else {
+    await defineClass.update(
+      {
+        name: reqClass.name,
+      },
+      {
+        where: {
+          classCode: reqClass.classCode,
+        },
+      }
+    );
+    LOG.info(`Class name updated.`);
+  }
+  return classes.id;
+};
+
+const updateTeacherSubject = async (teacherId: number, subjectId: number) => {
+  const [teacherSubject, created] = await defineTeacherSubject.findOrCreate({
+    where: { teacherId: teacherId, subjectId: subjectId },
+  });
+  if (created) {
+    LOG.info(`New teacher subject created.`);
+  } else {
+    LOG.info(`Teacher subject link existed.`);
+  }
+  return teacherSubject.id;
+};
+
+const updateTeacherClass = async (
+  teacherSubjectId: number,
+  classId: number
+) => {
+  const [teacherClass, created] = await defineTeacherClass.findOrCreate({
+    where: { TeacherSubjectId: teacherSubjectId, classId: classId },
+  });
+  if (created) {
+    LOG.info(`New teacher class created.`);
+  } else {
+    LOG.info(`Teacher class link existed.`);
+  }
+};
+
+const updateStudentClass = async (studentIds: number[], classId: number) => {
+  studentIds.forEach(async (studentId) => {
+    const [studentClass, created] = await defineStudentClass.findOrCreate({
+      where: { studentId: studentId, classId: classId },
+    });
+    if (created) {
+      LOG.info(`New student class created.`);
+    } else {
+      LOG.info(`Student class link existed.`);
+    }
+  });
+};
+
+RegistrationController.post('/', registrationHandler);
+
+export default RegistrationController;
--- typescript/src/router.ts
+++ typescript/src/router.ts
@@ -1,8 +1,10 @@
 import Express from 'express';
 import HealthcheckController from './controllers/HealthcheckController';
+import RegistrationController from './controllers/RegistrationController';
 
 const router = Express.Router();
 
 router.use('/', HealthcheckController);
+router.use('/register', RegistrationController);
 
 export default router;
